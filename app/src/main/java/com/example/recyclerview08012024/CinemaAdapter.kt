10fdd94b60747252120c8cdbce27789b9942b72d
kynguyen93 <ngocky15793@gmail.com>
2024-04-16 08:36:02 +0700

    onCreateViewHolder

---
+++ app/src/main/java/com/example/recyclerview08012024/CinemaAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.recyclerview08012024
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
+import androidx.recyclerview.widget.RecyclerView
+import java.util.zip.Inflater
+
+
+class CinemaAdapter(
+    var context: Context
+): RecyclerView.Adapter<CinemaAdapter.CinemaViewHolder>() {
+
+      class CinemaViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+      }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CinemaViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.layout_item_cinema, parent, false)
+        return CinemaViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: CinemaViewHolder, position: Int) {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/recyclerview08012024/TextUtils.kt
@@ -0,0 +1,4 @@
+package com.example.recyclerview08012024
+
+object TextUtils {
+}
