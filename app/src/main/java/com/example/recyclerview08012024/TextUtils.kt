7d8c2b344f7de4bb036efa942c63419266893cd4
kynguyen93 <ngocky15793@gmail.com>
2024-04-18 16:00:25 +0700

    fix test case

--- app/src/main/java/com/example/recyclerview08012024/TextUtils.kt
+++ app/src/main/java/com/example/recyclerview08012024/TextUtils.kt
@@ -1,17 +1,30 @@
 package com.example.recyclerview08012024
 
+import android.annotation.SuppressLint
+import kotlin.math.pow
+import kotlin.math.round
+
 object TextUtils {
 
+    @SuppressLint("SuspiciousIndentation")
     fun formatDistance(distance: Int): String{
         return if (distance < 1000) {
             "$distance m"
-        }else {
+        } else {
             val result = (distance.toFloat() / 1000.toFloat())
-            var digitZero = ((result * 10) % 10).toInt() == 0
-            if (result %1 != 0f && !digitZero) {
-                "${String.format("%.1f", result)} km"
-            } else {
-                "${result.toInt()} km"
+            var digitZero = ((result * 100) % 100).toInt() == 0
+            if (result % 1 != 0f && !digitZero) {
+                val cifras = 10.0.pow(2.0)
+                val finalResultString = String.format("%.1f", round(result * cifras) / cifras)
+                val finalResultFloat = finalResultString.toFloat()
+                if (finalResultFloat % 1.0f == 0f) {
+                    "${finalResultFloat.toInt()} km"
+                } else {
+                    "$finalResultString km"
+                }
+                } else {
+                    "${result.toInt()} km"
+
             }
         }
     }
